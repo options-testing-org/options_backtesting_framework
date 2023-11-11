130bbd182f0e061b9d2d7dd212b51c14919899e9
Ben McNeill <16538715+bfmcneill@users.noreply.github.com>
2023-11-11 14:53:18 -0700

    format with black

--- options_framework/config.py
+++ options_framework/config.py
@@ -3,10 +3,10 @@ from dynaconf import Dynaconf
 
 settings = Dynaconf(
     envvar_prefix="DYNACONF",
-    settings_files=['settings.toml', '.secrets.toml'],
+    settings_files=["settings.toml", ".secrets.toml"],
 )
 
 settings.PROJECT_ROOT = pathlib.Path(__file__).parent.parent
-settings.TEST_DATA_DIR = settings.PROJECT_ROOT / 'tests' / 'test_data'
+settings.TEST_DATA_DIR = settings.PROJECT_ROOT / "tests" / "test_data"
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
 # `settings_files` = Load these files in the order.
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,14 +1,19 @@
 import pathlib
-import pytest
-from dynaconf import Dynaconf
 from options_framework import config
+
 
 def test_project_root():
     assert config.settings.PROJECT_ROOT == pathlib.Path(__file__).parent.parent
+    assert (
+        config.settings.TEST_DATA_DIR
+        == config.settings.PROJECT_ROOT / "tests" / "test_data"
+    )
+
 
 def test_settings_toml_exists():
-    assumed_path = config.settings.PROJECT_ROOT / "settings.toml"
-    assert assumed_path.exists()
+    expected_settings_toml_path = config.settings.PROJECT_ROOT / "settings.toml"
+    assert expected_settings_toml_path.exists()
+
 
 def test_settings_toml_file_has_default_message():
     assert config.settings.DEFAULT_MESSAGE == "Default message"
